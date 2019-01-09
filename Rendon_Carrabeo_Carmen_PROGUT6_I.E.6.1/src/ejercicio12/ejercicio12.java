852548b7c945061378eab847c3afe1e475171ca1
carmen <carmen@carmenweb.com>
2019-01-09 23:55:59 +0100

    ejercicio12 no realizado

--- Rendon_Carrabeo_Carmen_PROGUT6_I.E.6.1/src/ejercicio12/ejercicio12.java
+++ Rendon_Carrabeo_Carmen_PROGUT6_I.E.6.1/src/ejercicio12/ejercicio12.java
@@ -7,14 +7,23 @@ import java.util.Random;
 import excepciones.trycatch;
 
 public class ejercicio12 {
+	static String[] lista =new String[5];
 
 	char[] abecedario= {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
-	char[][] matriz=new char[15][15];
-trycatch e= new trycatch();
-	Random random=new Random();
 	
-	public void iniciomatriz() {
+static trycatch e= new trycatch();
+	static Random random=new Random();
+	
+	public static void main(String[] args) throws IOException {
+		char[][] matriz=new char[15][15];
+		iniciomatriz(matriz);
+		perdirpalabra();
+		escondopalabra();
+	}
+	
+	
+	public static void iniciomatriz(char matriz[][]) {
 		for (int i = 0; i < matriz.length; i++) {
 
 			for (int j = 0; j < matriz[i].length; j++) {
@@ -22,37 +31,51 @@ trycatch e= new trycatch();
 				matriz[i][j]=' ';
 
 			}
-
+System.out.println(" ");
 		}
 	}
 	
 	
-	public void perdirpalabra() throws IOException {
-		ArrayList<String> lista = new ArrayList<String>(10);
+	public static void perdirpalabra() throws IOException {
+	String palabra;
 		System.out.println("Dame 10 palabras");
 	
-		for(int i=0; i<lista.size(); i++) {
-			lista.add(e.try_String());
+		for(int i=0; i<lista.length; i++) {
+			palabra=e.try_String();
+			lista[i]=palabra;
+			System.out.println("Dame otra palabra");
 		}
 		
 	}
-public void escondopalabra() {
-	
+public static void escondopalabra() {
 	int menu;
+	String palabra = null;
+	char[] chars = null;
+	for(int i =0; i<lista.length; i++) {
+		palabra=lista[i];
+	chars = palabra.toCharArray();
 	menu=random.nextInt(4);
+	switch(menu) {
+	case 1: vertical();
+	case 2: 
+	case 3:
+	}
+	
+	}
+	
+	
+
+	
+	
 	
 	
 }
 
-public void vertical()
-
-	public  void imprimirsopadeletras() {
-		for (int i = 0; i < matriz.length; i++) {
-
-			for (int j = 0; j < matriz[i].length; j++) {
-				matriz[i][j]=abecedario[random.nextInt(abecedario.length)];
-				System.out.println(matriz[i][j]);
-			}
-	}
-	}
+public void vertical() {
+	
 }
+
+
+	
+	}
+
