63c747a13b177ad3d67cfb957a40cc002dfd16aa
carmenrenca <carmenrenca3d@gmail.com>
2019-01-09 14:52:10 +0100

    ejercicio 12 no echo

--- Rendon_Carrabeo_Carmen_PROGUT6_I.E.6.1/src/ejercicio12/ejercicio12.java
+++ Rendon_Carrabeo_Carmen_PROGUT6_I.E.6.1/src/ejercicio12/ejercicio12.java
@@ -1,15 +1,27 @@
 package ejercicio12;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
+
+import excepciones.trycatch;
 
 public class ejercicio12 {
 
 	char[] abecedario= {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
 	char[][] matriz=new char[15][15];
-
+trycatch e= new trycatch();
 	Random random=new Random();
+	public void perdirpalabra() throws IOException {
+		ArrayList<String> lista = new ArrayList<String>(10);
+		System.out.println("Dame 10 palabras");
 	
+		for(int i=0; i<lista.size(); i++) {
+			lista.add(e.try_String());
+		}
+		
+	}
 	public void iniciomatriz() {
 		for (int i = 0; i < matriz.length; i++) {
 
