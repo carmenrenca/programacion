27c156863c7a1c04ce8e35663afc702693ffdb82
carmen <carmen@carmenweb.com>
2019-01-13 20:37:19 +0100

    ejercicio17

--- Rendon_Carrabeo_Carmen_PROGUT6_I.E.6.1/src/ejercicio17/ejercicio17.java
+++ Rendon_Carrabeo_Carmen_PROGUT6_I.E.6.1/src/ejercicio17/ejercicio17.java
@@ -7,15 +7,45 @@ import excepciones.trycatch;
 
 public class ejercicio17 {
 
+	static int n;
+	static int matriz[][] = new int[4][4];
+	static trycatch c= new trycatch();
+	
+	
+
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		trycatch c= new trycatch();
+		
 		arrays a= new arrays();
-		int n;
-System.out.println("dame el tamaño de la matriz");
-n=c.try_int();
-int matriz[][] = new int[n][n];
+
+rellenarmatriz(matriz);
+
 a.imprimirmatrizint(matriz);
 	}
+public static void rellenarmatriz(int matriz[][]) {
+	int j =0;
+	int i=0;
+	int contador = 0;
+	
+	
+	for( j=0; j<matriz.length; j++) {
+		contador++;
+		if(j<3) {
+			matriz[i][j+1]=contador;	
+		}
+	
+	}
+	j=3;
+	for(i=0;i<matriz.length; i++) {
+	if(j!=0) {
 
+	
+		matriz[i+1][j]=contador;
+		contador++;
+
+	
+}
+
+}
+}
 }
